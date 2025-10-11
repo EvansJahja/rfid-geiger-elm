7299d12103d2e6b3752b5368c539e4f706b5b008
Evans Jahja <evansjahja13@gmail.com>
2025-10-11 22:26:48 +0900

    cleanup Item

--- src/Item.elm
+++ src/Item.elm
@@ -1,4 +1,4 @@
-module Item exposing (..)
+module Item exposing (viewAddItemForm, viewEditItemForm, FormHiddenFields, Item, encoder, decoder)
 import Form
 import Form.Field as Field
 import Form.FieldView as FieldView
@@ -9,14 +9,10 @@ import Json.Encode
 import Json.Decode
 import EPC exposing (EPC)
 import Form.Validation as Validation
-import Browser
 import Html.Events exposing (onSubmit)
 import Html exposing (form)
 import Html.Attributes exposing (type_)
 import Html.Attributes exposing (disabled)
-import Html exposing (sub)
-import Svg exposing (image)
-import Form.Validation exposing (Validation)
 
 type alias Item =
     { title: String
@@ -24,8 +20,6 @@ type alias Item =
     , keywords: List String
     , imageDataUrl : Maybe String
     }
-
-type alias MySimpleType = {username : String}
 
 
 type alias FormHiddenFields =
@@ -139,25 +133,6 @@ addItemForm messages hidden errors submitting =
             ( Field.text
                 |> Field.withInitialValue (\_ ->  hidden.imageDataUrl)
             )
-
-
-errorsView :
-    Form.Context String input
-    -> Validation.Field String parsed kind
-    -> Html msg
-errorsView { submitAttempted, errors } field =
-    if submitAttempted || Validation.statusAtLeast Validation.Blurred field then
-        -- only show validations when a field has been blurred
-        -- (it can be annoying to see errors while you type the initial entry for a field, but we want to see the current
-        -- errors once we've left the field, even if we are changing it so we know once it's been fixed or whether a new
-        -- error is introduced)
-        errors
-            |> Form.errorsForField field
-            |> List.map (\error -> Html.li [ Html.Attributes.style "color" "red" ] [ Html.text error ])
-            |> Html.ul []
-
-    else
-        Html.ul [] []
 
 editItemForm : Messages msg ->  List String -> Bool -> Form.HtmlForm String Item Item msg
 editItemForm messages errors submitting = 
@@ -287,7 +262,7 @@ viewAddItemForm messages hiddenFields errors submitting formModel =
                     }
                     (
                     (Form.options "addItem"
-                        |> Form.withOnSubmit (\ {parsed} -> onSubmit parsed)
+                        |> Form.withOnSubmit (\{parsed} -> onSubmit parsed)
                     )
                     )
                     []
@@ -312,6 +287,31 @@ viewEditItemForm messages item errors submitting formModel =
                     )
                     []
 
+viewPanel : String -> List (Html msg) -> Html msg
+viewPanel title contents =
+    div [ class "p-4 border-base-300 rounded bg-base-100" ]
+        [ h1 [] [ text title ]
+        , div [ class "flex flex-col gap-2" ] contents
+        ]
+errorsView :
+    Form.Context String input
+    -> Validation.Field String parsed kind
+    -> Html msg
+errorsView { submitAttempted, errors } field =
+    if submitAttempted || Validation.statusAtLeast Validation.Blurred field then
+        -- only show validations when a field has been blurred
+        -- (it can be annoying to see errors while you type the initial entry for a field, but we want to see the current
+        -- errors once we've left the field, even if we are changing it so we know once it's been fixed or whether a new
+        -- error is introduced)
+        errors
+            |> Form.errorsForField field
+            |> List.map (\error -> Html.li [ Html.Attributes.style "color" "red" ] [ Html.text error ])
+            |> Html.ul []
+
+    else
+        Html.ul [] []
+
+-- ENCODER / DECODER
 
 encoder : Item -> Json.Encode.Value
 encoder item =
@@ -344,170 +344,3 @@ decoder =
             (Json.Decode.field "epc" epcDecoder)
             (Json.Decode.field "keywords" (Json.Decode.list Json.Decode.string))
             (Json.Decode.field "imageDataUrl" (Json.Decode.nullable Json.Decode.string))
-
-
-
-type alias Form =
-    { title : String
-    , epc : String
-    , keywords : List String
-    , imageDataUrl : Maybe String
-    }
-
-type alias ValidationErrors =
-    { title : List String
-    , epc : List String
-    , formErrors : List String
-    }
-emptyValidationErrors : ValidationErrors
-emptyValidationErrors =
-    { title = []
-    , epc = []
-    , formErrors = []
-    }
-
-defaultForm  : Form
-defaultForm  =
-    { title = ""
-    , epc = ""
-    , keywords = []
-    , imageDataUrl = Nothing
-    }
-
-type Error 
-    = TitleRequired
-    | EPCRequired
-    | EPCLengthInvalid
-
--- title : Decoder String Error String
--- title = 
---     Decoder.identity
---         |> Decoder.assert (Decoder.minLength TitleRequired 1)
-    
--- title_ : Decoder Form Error String
--- title_ =
---     Decoder.lift .title title
-
--- epc : Decoder String Error EPC
--- epc =
---     Decoder.identity
---         |> Decoder.assert (Decoder.minLength EPCRequired 1)
---         |> Decoder.assert (Decoder.minLength EPCLengthInvalid 24)
---         |> Decoder.assert (Decoder.maxLength EPCLengthInvalid 24)
---         |> Decoder.andThen
---             (\s ->
---                 -- Decoder.always (EPC.EPC [])
---                 case (EPC.epcStringPrism.getOption s) of
---                     Just e -> Decoder.always e
---                     Nothing -> Decoder.fail EPCLengthInvalid
---             )
-
--- epc_ : Decoder Form Error EPC
--- epc_ =
---     Decoder.lift .epc epc
-
--- keywords : Decoder (List String) Error (List String)
--- keywords =
---     Decoder.identity
-
--- keywords_ : Decoder Form Error (List String)
--- keywords_ =
---     Decoder.lift .keywords keywords
-
--- imageDataUrl : Decoder (Maybe String) Error (Maybe String)
--- imageDataUrl =
---     Decoder.identity
-
--- imageDataUrl_ : Decoder Form Error (Maybe String)
--- imageDataUrl_ =
---     Decoder.lift .imageDataUrl imageDataUrl
-
--- form : Decoder Form Error Item
--- form =
---     Decoder.top Item
---         |> Decoder.field title_
---         |> Decoder.field epc_
---         |> Decoder.field keywords_
---         |> Decoder.field imageDataUrl_
-
-
--- decodeForm : Form -> Result (List Error) Item
--- decodeForm = Decoder.run form
-
--- itemFormError f input = 
---     let
---         fieldDecoders =
---             { title = title
---             , epc = epc
---             }
-
---         field = f fieldDecoders
---     in
---         List.map errorToString (Decoder.errors field input)
-
-
--- titleErrorField : String -> Html msg
--- titleErrorField input =
---     div
---         [ class "error"
---         ]
---         <| List.map errorText
---             (Decoder.errors title input)
-
--- errorText : Error -> Html msg
--- errorText err =
---     p
---         [ class "errorText"
---         ]
---         [ text (errorToString err)
---         ]
--- errorToString : Error -> String
--- errorToString err =
---     case err of
---         TitleRequired ->
---             "Title is required"
---         EPCRequired ->
---             "EPC is required"
---         EPCLengthInvalid ->
---             "EPC must be 24 characters long"
-
-
--- view : MyTestModel -> Html Msg
--- view m =
---     myRenderedForm m.formState
-
-
--- type alias MyTestModel =
---     { formState : Form.Model
---     }
--- initModel : MyTestModel
--- initModel =
---     { formState = Form.init
---     }
-
--- update : Msg -> MyTestModel -> (MyTestModel, Cmd Msg)
--- update msg model =
---     case msg of
---         FormMsg formMsg ->
---             let
---                 ( updatedFormModel, cmd ) =
---                     Form.update formMsg model.formState
---             in
---             ( { model | formState = updatedFormModel }, cmd )
-            
-
--- main : Program () MyTestModel Msg
--- main =
---     Browser.element
---         { init = \_ -> ( initModel, Cmd.none )
---         , view = view
---         , update = update
---         , subscriptions = \_ -> Sub.none
---         }
-
-viewPanel : String -> List (Html msg) -> Html msg
-viewPanel title contents =
-    div [ class "p-4 border-base-300 rounded bg-base-100" ]
-        [ h1 [] [ text title ]
-        , div [ class "flex flex-col gap-2" ] contents
-        ]
